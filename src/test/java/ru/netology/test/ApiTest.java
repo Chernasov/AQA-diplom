efe6ff055d547256d5f2d15665dcf89490bc645b
Timofey Chernasov <timofeychernasov@gmail.com>
2021-12-18 18:41:32 +0300

    Fix API test

--- src/test/java/ru/netology/test/ApiTest.java
+++ src/test/java/ru/netology/test/ApiTest.java
@@ -1,15 +1,27 @@
 package ru.netology.test;
 
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import ru.netology.data.DataHelper;
 import ru.netology.page.ApiPage;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static ru.netology.data.DBaseQueries.getOrder;
-import static ru.netology.data.DBaseQueries.getPayment;
 
 public class ApiTest {
     private ApiPage api = new ApiPage();
+
+    @BeforeAll
+    static void setUpAll() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+    }
+
+    @AfterAll
+    static void tearDownAll() {
+        SelenideLogger.removeListener("allure");
+    }
 
     @Test
     void shouldUseApprovedCardForPay() {
